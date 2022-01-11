08327764f48e40e7f1feeaac0ec25e36347261ba
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2022-01-11 12:20:16 -0800

    day1 solution

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,20 @@
 def initialize_app 
-    puts 'welcome to phase-3'
+    movies = ["batman", "superman", "spider-man", "ironman"]
+    # puts 'welcome to phase-3'
+    
+    puts "Welcome to Flatiron Movies"
+    puts "Please choose an option:"
+    puts "1. List all Movies"
+    puts "0. to exit"
+    # Max's solution
+    # user_input = gets.strip
+    # case user_input
+    # when "1"
+    #     movies.each{|movie| puts movie}
+    # else
+    #     puts "goodbye"
+    # end
+
 end 
 
 
