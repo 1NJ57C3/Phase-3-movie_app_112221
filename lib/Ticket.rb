6892bf5edd596f0b364ce653cd47cca9e4bb7a25
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2022-01-12 11:20:23 -0800

    day02wip

---
+++ lib/Ticket.rb
@@ -0,0 +1,15 @@
+class Ticket
+    attr_accessor :name, :title, :price
+    @@all = []
+
+    def intialize(name, title)
+        @name = name
+        @title = title
+        @price = 5.00
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+end
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,10 +1,14 @@
-def initialize_app 
-    movies = ['2001: A Space Odyssey', 'The Godfather', 'Citizen Kane', 'Raiders of the Lost Ark', 'In the mood for love', 'Seven Samurai']
+require 'pry'
 
+def initialize_app 
+    menu
+end
+
+def menu
     puts "Welcome to Flatiron Movies"
     puts "Please choose an option:"
     puts "1. List all Movies"
-    puts "2. List Tickte holder names"
+    puts "2. List Ticket holder names"
     puts "3. Add ticket"
     puts "0. to exit"
     
@@ -12,9 +16,11 @@ def initialize_app
 
     case user_input
     when "1"
+        movies = ['2001: A Space Odyssey', 'The Godfather', 'Citizen Kane', 'Raiders of the Lost Ark', 'In the mood for love', 'Seven Samurai']
         movies.each {|movie| puts movie }
         
     when "2"
+        Ticket.all.each { |ticket| puts ticket[:name]}
 
     when "3"
         puts create_ticket
@@ -29,7 +35,14 @@ def initialize_app
 end 
 
 def create_ticket
-   
+    puts "What is your name?"
+    name = gets.strip
+    puts "What is the movie title?"
+    title = gets.strip
+
+    t1 = Ticket.new(name, title)
+    binding.pry
+    menu
 end 
 
 
